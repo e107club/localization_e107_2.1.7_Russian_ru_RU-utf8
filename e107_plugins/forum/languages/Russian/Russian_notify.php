0744b3eab69baf6bcbc3f35be1be9d9f9d52a462
net1313 <net1313@gmail.com>
2017-12-28 14:08:06 +0100

    Pre-release

---
+++ e107_plugins/forum/languages/Russian/Russian_notify.php
@@ -0,0 +1,17 @@
+<?php
+
+// Bing-Translated Language file 
+// Generated for e107 v2.x by the Multi-Language Plugin
+// https://github.com/e107inc/multilan
+
+define("LAN_FORUM_NT_7", "Форум - тема создана новым пользователем");
+define("LAN_FORUM_NT_8", "Форум - Тема удалена");
+define("LAN_FORUM_NT_9", "Форум - Темы объеденены");
+define("LAN_FORUM_NT_10", "Форум - Сообщение удалено");
+define("LAN_FORUM_NT_11", "Форум - Сообщение создано");
+define("LAN_FORUM_NT_NEWTOPIC", "Новая тема создана");
+define("LAN_FORUM_NT_NEWTOPIC_PROB", "Новая тема, создан испытательный срок члена");
+define("LAN_FORUM_NT_TOPIC_DELETED", "Тема удалена");
+define("LAN_FORUM_NT_TOPIC_SPLIT", "Тема разделена");
+define("LAN_FORUM_NT_POST_DELETED", "Сообщение удалено");
+define("LAN_FORUM_NT_POST_REPORTED", "Сообщение создано");
